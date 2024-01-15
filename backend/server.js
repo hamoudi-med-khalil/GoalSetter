f7b9fd3df64053dfddfd85ffe9b75b69f4084255
hamoudi med khalil <mohamedkhalilhamoudi1@gmail.com>
2024-01-15 14:00:06 +0100

    Initial Rest API for goals

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,14 @@ const express = require ('express')
 const dotenv = require ('dotenv').config()
 const colors = require ('colors')
 const {errorHandler} = require('./middelware/errorMiddelware')
+const connectDB = require('./config/db')
 const port = process.env.port || 5000
 
+
+connectDB()
+
 const app = express()
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
