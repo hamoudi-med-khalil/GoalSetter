461bbfdb2c6079cdf9ed5112a2efc07979704aa1
hamoudi med khalil <mohamedkhalilhamoudi1@gmail.com>
2024-01-23 09:28:59 +0100

    Hook up Register & Login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
-
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
     const [formData, setFormData] = useState({ 
@@ -8,6 +12,25 @@ function Login() {
         password: ''
     })
     const { email, password } = formData
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const { user, isError, isSuccess, isLoading, message} = useSelector(
+        (state) => state.auth)
+
+        useEffect(() =>{
+            if(isError){
+                toast.error(message)
+            }
+            if(isSuccess || user){
+                navigate('/')
+            }
+            dispatch(reset())
+    
+        },[user, isError, isSuccess, message, navigate, dispatch])
+
+
 
     const onChange = (e) => {
         setFormData((prevState)=> ({
@@ -17,8 +40,18 @@ function Login() {
      }
     const onSubmit = (e) => {
         e.preventDefault()
+        
+        const userData = {
+            email,
+            password
+        }
+        dispatch(login(userData))
+
     }
 
+    if(isLoading){
+        return <Spinner />
+    }
 
 
     return <>
